cff1b754874e1b019fedc77baefcf49a12488b8b
Ripa Aloyan <Hripsime_Aloyan@epam.com>
2021-03-17 19:44:24 +0400

    Add calculator Project

---
+++ calculator/src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+public abstract class Calculator {
+    public static double add (double a, double b) {
+        return a+b;
+    }
+
+    public static double sub (double a, double b) {
+        return a-b;
+    }
+
+    public static double mul (double a, double b) {
+        return a*b;
+    }
+
+    public static double div (double a, double b) throws Exception {
+        if(b==0){
+            throw new ArithmeticException("A number cannot be divided into 0");
+        }
+        return a/b;
+    }
+}
+
