ac2550295da23c40e79184aba9c775890a3e6c15
Ripa Aloyan <Hripsime_Aloyan@epam.com>
2021-03-24 22:53:01 +0400

    artifactId - lowercase and chages in Test code

--- WebsiteGreetz/src/test/java/WebsiteTest.java
+++ WebsiteGreetz/src/test/java/WebsiteTest.java
@@ -8,8 +8,9 @@ import org.testng.annotations.Test;
 
 public class WebsiteTest {
     private WebDriver driver;
-    private void logIn(WebDriver driver) throws InterruptedException {
-
+    private void logIn() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.greetz.nl/auth/login");
         Thread.sleep(3000);
@@ -27,37 +28,31 @@ public class WebsiteTest {
         return driver.findElements(By.className("full-size")).size() > countOfFavorites;
     }
 
-
     @Test
     public void addAndCheckFavorite() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        logIn(driver);
+        logIn();
 
         //եթե կա, ջնջենք
         driver.get("https://www.greetz.nl/mygreetz/favorieten");
         Thread.sleep(5000);
         if(driver.findElement(By.xpath("//button[@class='b-icon_medium b-icon-delete']")).isDisplayed()) {
-            WebElement deleteFavorite = driver.findElement(By.xpath("//button[@class='b-icon_medium b-icon-delete']"));
-            deleteFavorite.click();
+            WebElement deleteConfirmationButton = driver.findElement(By.xpath("//button[@class='b-icon_medium b-icon-delete']"));
+            deleteConfirmationButton.click();
             WebElement okDelete = driver.findElement(By.xpath("//span[contains(text(), 'Ok')]"));
             okDelete.click();
             }
-
-
-
         driver.get("https://www.greetz.nl/ballonnen/denken-aan");
         Thread.sleep(3000);
         WebElement targetItem = driver.findElement(By.xpath("//a[@data-id='1142804226']"));
 
-        WebElement targetElementFavoriteItem = targetItem.findElement(By.xpath("//parent::div//child::a[@class='b-products-grid__item-action']"));
-        targetElementFavoriteItem.click();
+        WebElement targetElementFavouritebutton = targetItem.findElement(By.xpath("//parent::div//child::a[@class='b-products-grid__item-action']"));
+        targetElementFavouritebutton.click();
 
         WebElement sideMenu = driver.findElement(By.xpath("//i[@class='page-header__navigation-item-icon b-icon b-icon-hamburger']"));
         sideMenu.click();
 
-        WebElement favorite = driver.findElement(By.xpath("//span[@class='b-list--item-subject' and contains(text(), 'Favorieten')]"));
-        favorite.click();
+        WebElement favourite = driver.findElement(By.xpath("//span[@class='b-list--item-subject' and contains(text(), 'Favorieten')]"));
+        favourite.click();
 
         int countOfFavorites = 0;
         Assert.assertTrue(findFavorite(countOfFavorites));
@@ -66,10 +61,7 @@ public class WebsiteTest {
 
     @Test
     public void priceForTwoCards() throws InterruptedException {
-
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        logIn(driver);
+        logIn();
 
         driver.get("https://www.greetz.nl/kaarten/denken-aan");
         Thread.sleep(5000);
